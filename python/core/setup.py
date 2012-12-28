43bdb21ce6a45fb6aa4ea16be04ba02a88e09d03
William Mayor <mail@williammayor.co.uk>
2012-12-28 17:39:18 +0000

    Moved cli and alfred to core scripts Rather than have separate packages for each the alfred and cli scripts are now just included as scripts. No mess, no fuss.

--- python/core/setup.py
+++ python/core/setup.py
@@ -5,10 +5,12 @@ setup(
     version='0.1.2dev',
     author='William Mayor',
     author_email='mail@williammayor.co.uk',
-    packages=['pinschercore', 'pinschercore.test', ],
     url='http://pinscher.williammayor.co.uk',
     license='LICENSE.txt',
     description='Core utilities for interacting with pinscher password files',
     long_description=open('README.txt').read(),
     install_requires=['pycrypto', ],
+    packages=['pinschercore', 'pinschercore.test', ],
+    test_suite='pinschercore.test',
+    scripts=['scripts/pinschercli', 'scripts/pinscheralfred']
 )
